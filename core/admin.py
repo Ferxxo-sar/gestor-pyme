d405c19147aeb1517b8fb92d981d60458f3326f9
Fernando Sarquis <ferfsarquis@gmail.com>
2025-10-16 19:28:22 -0300

    feat: panel basico otorgado por django con controles de administrador

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# importo todos los modelos
+from .models import Categoria, Proveedor, Producto, Venta, DetalleVenta
+
+# otorgo permisos de administrador
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+# Importo el user que provee Django
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.nombre
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    stock_minimo = models.PositiveIntegerField(default=5)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return self.nombre
+
+
+class Venta(models.Model):
+    # esto guarda la fecha de venta automaticamente y hace que no se pueda eliminar el registro
+    fecha = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    # relacion 1 (user) N (ventas) Ademas tiene que ser protect porque django elimina en cascada
+    vendedor = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Venta #{self.id} por {self.vendedor.username} - {self.fecha.strftime('%Y-%m-%d')}"
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.PROTECT)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # calculo de venta y guardado en bdd
+    def save(self, *args, **kwargs):
+        self.subtotal = self.cantidad * self.precio_unitario
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.producto.nombre}"
