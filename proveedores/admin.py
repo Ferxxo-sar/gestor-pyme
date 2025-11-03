c03c63d27bda560dada3ff001e1e88319a94e46e
Juliantes <poroto_vsc@hotmail.com>
2025-11-02 22:07:49 -0300

    Se agregan datos y tipo al proveedor, se cargan proveedores al servidor

--- proveedores/admin.py
+++ proveedores/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Proveedor
+from .models import Proveedor, TipoProveedor
+
+@admin.register(TipoProveedor)
+class TipoProveedorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion')
+    list_display_links = ('id', 'nombre')
+    search_fields = ('nombre',)
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'telefono', 'email')
+    list_display = ('id', 'nombre', 'telefono', 'email', 'tipo')
     list_display_links = ('id', 'nombre')
     search_fields = ('nombre', 'email')
+    list_filter = ('tipo',)
--- proveedores/models.py
+++ proveedores/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+class TipoProveedor(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.nombre
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=200, unique=True)
     telefono = models.CharField(max_length=20, blank=True)
     email = models.EmailField(blank=True, null=True)
+    direccion = models.CharField(max_length=300, blank=True, null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    tipo = models.ForeignKey(TipoProveedor, on_delete=models.SET_NULL, null=True, blank=True)
+    
     
     def __str__(self):
         return self.nombre
